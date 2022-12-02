b85b84db3513db0723d78a8595c99331f79d5f37
xtz <mail@xtz.com>
2022-12-02 20:23:39 +0800

    letsencrypt is used by default https://github.com/acmesh-official/acme.sh/wiki/Change-default-CA-to-ZeroSSL

--- trojan_install.sh
+++ trojan_install.sh
@@ -175,6 +175,7 @@ EOF
 	#申请https证书
 	mkdir /usr/src/trojan-cert /usr/src/trojan-temp
 	curl https://get.acme.sh | sh
+    ~/.acme.sh --set-default-ca --server letsencrypt
 	~/.acme.sh/acme.sh  --issue  -d $your_domain  --standalone
     	~/.acme.sh/acme.sh  --installcert  -d  $your_domain   \
         --key-file   /usr/src/trojan-cert/private.key \
